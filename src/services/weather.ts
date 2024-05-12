22d3742fa7b75ac5c16001ade8866153a4ca17af
Clement Meyer <meyclem@gmail.com>
2024-05-12 13:43:08 +0200

    add weather service

---
+++ src/services/weather.ts
@@ -0,0 +1,100 @@
+import { z } from "zod";
+
+enum CONDITIONS {
+  ATMOSPHERE = "Atmosphere",
+  CLEAR = "Clear",
+  CLOUDS = "Clouds",
+  DRIZZLE = "Drizzle",
+  MIST = "Mist",
+  RAIN = "Rain",
+  SNOW = "Snow",
+  THUNDERSTORM = "Thunderstorm",
+}
+
+const ConditionsEnum = z.nativeEnum(CONDITIONS);
+type Condition = z.infer<typeof ConditionsEnum>;
+
+type Weather = {
+  weather: Condition;
+  temp: number;
+};
+
+class WeatherAPIError extends Error {}
+
+async function getCoordinates(city: string): Promise<{ lat: number; lon: number }> {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`,
+  );
+
+  if (response.status !== 200) {
+    throw new WeatherAPIError("Failed to fetch latitude and longitude");
+  }
+
+  const body = await response.json();
+
+  const result = z
+    .array(
+      z.object({
+        lat: z.number(),
+        lon: z.number(),
+      }),
+    )
+    .safeParse(body);
+
+  if (!result.success) {
+    throw new WeatherAPIError("Failed to parse lat and lon");
+  }
+
+  const [{ lat, lon }] = result.data;
+
+  return { lat, lon };
+}
+
+async function getWeatherFromLatLon(lat: number, lon: number): Promise<Weather> {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`,
+  );
+
+  if (response.status !== 200) {
+    throw new WeatherAPIError("Failed to fetch weather for location");
+  }
+
+  const body = await response.json();
+
+  const weatherData = z
+    .object({
+      weather: z.array(
+        z.object({
+          main: ConditionsEnum,
+        }),
+      ),
+      main: z.object({
+        temp: z.number(),
+      }),
+    })
+    .safeParse(body);
+
+  if (!weatherData.success) {
+    throw new WeatherAPIError("Failed to parse weather data");
+  }
+
+  return {
+    weather: weatherData.data.weather[0].main,
+    temp: weatherData.data.main.temp,
+  };
+}
+
+/**
+ * Retrieves weather information based on the provided city.
+ * @param city - The name of the city.
+ * @returns A Promise that resolves to the weather information for the city.
+ * @throws {WeatherAPIError} If the weather API returns an error.
+ */
+async function getFromLocation(city: string): Promise<Weather> {
+  const { lat, lon } = await getCoordinates(city);
+
+  return getWeatherFromLatLon(lat, lon);
+}
+
+export { getFromLocation, CONDITIONS, WeatherAPIError };
+export type { Condition, Weather };
---
+++ tests/unit/weather.spec.ts
@@ -0,0 +1,77 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { getFromLocation, WeatherAPIError } from "../../src/services/weather";
+
+import type { MockedFunction } from "vitest";
+
+global.fetch = vi.fn();
+
+function createFetchResponse(data = {}, status = 200) {
+  return { json: () => new Promise((resolve) => resolve(data)), status };
+}
+
+function mockFetch(data = {}, status = 200) {
+  (fetch as MockedFunction<any>).mockResolvedValueOnce(createFetchResponse(data, status));
+}
+
+describe("Weather Service", () => {
+  describe("#getFromLocation", () => {
+    afterEach(() => {
+      (fetch as MockedFunction<any>).mockClear();
+    });
+
+    it("Throws an error if the geocode API doesn't respond with a 200", async () => {
+      expect.assertions(2);
+      mockFetch({}, 401);
+
+      try {
+        await getFromLocation("New York");
+      } catch (error) {
+        expect(error).toBeInstanceOf(WeatherAPIError);
+        expect(error.message).toEqual("Failed to fetch latitude and longitude");
+      }
+    });
+
+    it("Throws an error if the weather API doesn't respond with a 200", async () => {
+      expect.assertions(2);
+      mockFetch([{ lat: 51.5073219, lon: -0.1276474 }], 200);
+      mockFetch({}, 401);
+
+      try {
+        await getFromLocation("New York");
+      } catch (error) {
+        expect(error).toBeInstanceOf(WeatherAPIError);
+        expect(error.message).toEqual("Failed to fetch weather for location");
+      }
+    });
+
+    it("Responds with the right weather data for a given location", async () => {
+      expect.assertions(1);
+      const london = [
+        {
+          lat: 51.5073219,
+          lon: -0.1276474,
+        },
+      ];
+
+      const weather = {
+        weather: [
+          {
+            id: 501,
+            main: "Rain",
+          },
+        ],
+        main: {
+          temp: 29.48,
+        },
+      };
+      mockFetch(london, 200);
+      mockFetch(weather);
+      const result = await getFromLocation("London");
+
+      expect(result).toEqual({
+        weather: weather.weather[0].main,
+        temp: weather.main.temp,
+      });
+    });
+  });
+});
